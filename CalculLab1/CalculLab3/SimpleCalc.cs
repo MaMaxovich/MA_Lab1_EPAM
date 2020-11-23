134612121b5024736112037de694a1ceec160e4d
MaMaxovich <makap311@ya.ru>
2020-11-23 13:15:47 +0300

    Файлы проектов С#
    Кракен не дал создать отдельные репозитории (репозитории создались, но их не видно из кракена...((...   )

---
+++ CalculLab1/CalculLab3/SimpleCalc.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculLab3
+{
+    class SimpleCalc
+    {
+        public double leftNumber = 0;
+        public double rightNumber = 0;
+        public string calcOperator = "+";
+        public double calcResult = 0;
+        public bool LeftNumber()
+        {
+            bool result = true;
+            while (1 > 0)
+            {
+                Console.WriteLine("Ввести первое число");
+                try
+                {
+                    leftNumber = double.Parse(Console.ReadLine());
+                    break;
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message + ": Ввести первое число цифрами");
+                    result = false;
+                }
+            }
+            return result;
+        }
+        public bool RightNumber()
+        {
+            bool result = true;
+            while (1 > 0)
+            {
+                Console.WriteLine("Ввести второе число");
+                try
+                {
+                    rightNumber = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message + ": Ввести второе число цифрами");
+                    result = false;
+                }
+            }
+            return result;
+        }
+        public bool CalcOperator()
+        {
+            bool result = false;
+            while (1 > 0)
+            {
+                Console.WriteLine("Выбрать действие:  + - / *");
+
+                calcOperator = Console.ReadLine();
+                switch (calcOperator)
+                {
+                    case "+":
+                        result = true;
+                        break;
+                    case "-":
+                        result = true;
+                        break;
+                    case "/":
+                        result = true;
+                        break;
+                    case "*":
+                        result = true;
+                        break;
+                }
+                if (result == true) break;
+            }
+            return result;
+        }
+        public bool CalcResult()
+        {
+            bool result = true;
+
+            try
+            {
+                switch (calcOperator)
+                {
+                    case "+":
+                        calcResult = leftNumber + rightNumber;
+                        Console.WriteLine("Результат сложения: {0}", calcResult.ToString());
+                        break;
+                    case "-":
+                        calcResult = leftNumber - rightNumber;
+                        Console.WriteLine("Результат вычитания: {0}", calcResult.ToString());
+                        break;
+                    case "/":
+                        calcResult = leftNumber / rightNumber;
+                        Console.WriteLine("Результат деления: {0}", calcResult.ToString());
+                        break;
+                    case "*":
+                        calcResult = leftNumber * rightNumber;
+                        Console.WriteLine("Результат умножения: {0}", calcResult.ToString());
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CalculLab1/Inheritance/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Iphone iphone = new Iphone
+            {
+                Name = "Мой IPhone 12PRO",
+                Lenght = 12,
+                Width = 6,
+                OS = "IOS 14"
+            };
+            iphone.ShowScreen();
+            Console.WriteLine(iphone.ToString());
+            Console.ReadLine();
+
+            Tamagychi pet = new Tamagychi
+            {
+                Name = "Тузик",
+                Lenght = 4,
+                Width = 4,
+                OS = "TamagOS"
+            };
+            pet.ShowScreen();
+            pet.Feed();
+            pet.Feed();
+            pet.Feed();
+            Console.ReadLine();
+
+            Tamagychi petClone = new Tamagychi
+            {
+                Name = "Тузик",
+                Lenght = 5,
+                Width = 4,
+                OS = "TamagOS"
+            };
+            Console.WriteLine("{0} и его клон {1}", pet.Name, pet.Equals(petClone) ? "равны": "не равны") ;
+
+            petClone.Lenght = 4;
+            petClone.Feed();
+            petClone.Feed();
+            petClone.Feed();
+
+
+            Console.WriteLine("{0} и его клон {1}", pet.Name, pet.Equals(petClone) ? "равны" : "не равны");
+
+        }
+    }
+}
